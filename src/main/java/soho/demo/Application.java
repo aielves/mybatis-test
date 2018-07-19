2d3021c68be0e2dc192fceb7268f5ae3483feb0c
chenjianqiang <chenjianqiang@192.168.1.251>
2018-07-19 12:18:20 +0800

    1.新增readme.txt

--- src/main/java/soho/demo/Application.java
+++ src/main/java/soho/demo/Application.java
@@ -1,6 +1,11 @@
 package soho.demo;
 
+import com.soho.mybatis.crud.aconst.MODE;
 import com.soho.mybatis.exception.BizErrorEx;
+import com.soho.mybatis.sqlcode.condition.Cnd;
+import com.soho.mybatis.sqlcode.condition.imp.SQLCnd;
+import com.soho.mybatis.sqlcode.domain.Join;
+import freemarker.template.utility.DateUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
@@ -9,9 +14,13 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import soho.demo.domain.RbacRole;
+import soho.demo.domain.RbacUser;
+import soho.demo.service.RbacRoleService;
 import soho.demo.service.RbacUserService;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Controller
@@ -26,11 +35,16 @@ public class Application {
     @ResponseBody
     @RequestMapping("/test")
     public Object test() throws BizErrorEx {
-        System.out.println(rbacUserService);
         Map<String, Object> map = new HashMap<>();
         map.put("username", "张三");
         map.put("password", "123456");
-        map.put("friends", rbacUserService.countAll());
+//        Cnd sql1 = new SQLCnd().eq("username", "zhangsan").gt("age", 18);
+//        Cnd sql2 = new SQLCnd().between("ctime", 1531843200000l, 1531929600000l).notnull("email");
+//        Cnd sql3 = new SQLCnd().or(sql1, sql2).in("state", 1, 2, 3);
+//        List<RbacUser> rbacUsers = rbacUserService.findByCnd(sql3);
+
+        Cnd sql = new SQLCnd().fields("a.id", "a.name", "a.code").from("rbac_role a").join(new Join(MODE.LEFT, "rbac_user_role b").on("a.id", "b.roleId")).join(new Join(MODE.LEFT, "rbac_user c").on("b.userId", "c.id")).eq("c.id", 1l);
+        List<RbacRole> rbacRoles = rbacUserService.findMapByCnd(sql, RbacRole.class);
         return map;
     }
 
---
+++ src/main/resources/readme.txt
@@ -0,0 +1,19 @@
+1.项目架构
+Maven
+Jdk1.8+
+Sprong Boot2.0+
+
+2.源码结构
+src java源代码
+resource 资源文件(包含数据库文件,soho-mybatis.jar)
+pom.xml Maven项目构建文件
+
+3.CRUD代码生成,无需自己编写CRUD代码,请前往生成器平台免费注册使用
+生成器平台地址: https://www.cartoonai.com
+
+3.版权说明
+soho-mybatis的版权归原作者所有，如有侵犯著作权请与本人联系
+
+4.联系作者
+昵称: 小丑哥
+QQ: 124010356
